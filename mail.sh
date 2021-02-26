506a17eb6c3719313afdb0167f9a6ec8e215870c
Fusity <44893830+Fusity@users.noreply.github.com>
2021-02-26 16:05:49 +0100

    Added "if" to check if ip is different from last

--- mail.sh
+++ mail.sh
@@ -1,7 +1,19 @@
-###################configure IP###########################
+###################configure IP##########################################################
 
 _IP =$(hostname -I) || true
 
-###################send mail###################################
+###################send mail#############################################################
 
-echo "Mon adresse ip est" "$_IP" | mail -s "Adresse IP du RPi" hotfirenet+srvweb@gmail.com
+mail = 0
+
+if [ _IP != mail ];
+then
+  echo "My ip address is" "$_IP" | mail -s "IP address of RPi" hotfirenet+srvweb@gmail.com
+fi
+
+mail = _IP
+
+##################this lines is for configure then is maybe deleted#######################
+
+sed -i '$d' ./mail.sh #Delete the last line first time and second time delete this line
+sed -i '7d' ./mail.sh #Delete the 7th line
