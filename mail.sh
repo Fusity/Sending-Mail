6d78ea2c7c23f4f1224c16a9877abe87d1486649
Fusity <44893830+Fusity@users.noreply.github.com>
2021-04-19 11:10:35 +0200

    Update mail.sh
    Deleting lines

--- mail.sh
+++ mail.sh
@@ -4,16 +4,12 @@ _IP =$(hostname -I) || true
 
 ###################send mail#############################################################
 
-mail = 0
+current_ip = 0
 
-if [ _IP != mail ];
+if [ _IP != current_ip ];
 then
   echo "My ip address is" "$_IP" | mail -s "IP address of RPi" hotfirenet+srvweb@gmail.com
 fi
 
-mail = _IP
-
-##################this lines is for configure then is maybe deleted#######################
-
-sed -i '$d' ./mail.sh #Delete the last line first time and second time delete this line
-sed -i '7d' ./mail.sh #Delete the 7th line
+current_ip = _IP
+echo current_ip > ip
